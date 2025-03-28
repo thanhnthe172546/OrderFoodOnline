25682aede4a11cbd4078ae4d7437025a299c596c
Thanhnthe <23trongthanh@gmail.com>
2025-03-28 23:12:52 +0700

    update dashboard backend 1

--- app/src/main/java/com/example/projectbl5/Adapter/BestFoodAdapter.java
+++ app/src/main/java/com/example/projectbl5/Adapter/BestFoodAdapter.java
@@ -4,10 +4,15 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.example.projectbl5.Domain.Food;
 import com.example.projectbl5.R;
 
@@ -24,23 +29,38 @@ public class BestFoodAdapter extends RecyclerView.Adapter<BestFoodAdapter.ViewHo
     @Override
     public BestFoodAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         context = parent.getContext();
-        View inflate = LayoutInflater.from(context).inflate(R.layout.viewholder_best_food,parent,false);
-        return null;
+        View inflate = LayoutInflater.from(context).inflate(R.layout.viewholder_best_deal,parent,false);
+        return new ViewHolder(inflate) ;
     }
 
     @Override
     public void onBindViewHolder(@NonNull BestFoodAdapter.ViewHolder holder, int position) {
+        holder.titleTxt.setText(foods.get(position).getTitle());
+        holder.starTxt.setText(" "+foods.get(position).getStar());
+        holder.timeTxt.setText(foods.get(position).getTimeValue()+" min");
+        holder.priceTxt.setText("$ "+foods.get(position).getPrice());
 
+        Glide.with(context)
+                .load(foods.get(position).getImagePath())
+                .transform(new CenterCrop(), new RoundedCorners(30))
+                .into(holder.pic);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return foods.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTxt,starTxt,timeTxt,priceTxt;
+        ImageView pic;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            titleTxt = itemView.findViewById(R.id.titleTxt);
+            starTxt = itemView.findViewById(R.id.starTxt);
+            timeTxt = itemView.findViewById(R.id.timeTxt);
+            priceTxt = itemView.findViewById(R.id.priceTxt);
+            pic = itemView.findViewById(R.id.pic);
         }
     }
 }
